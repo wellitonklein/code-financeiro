5b7d675c1ec206d197f991348c3dddb38ff01b6f
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-08-02 12:53:39 -0300

    Configuração do webpack-dev-server

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
+const gulp = require('gulp')
 const elixir = require('laravel-elixir')
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
 const webpackConfig = require('./webpack.config')
 const webpackDevConfig = require('./webpack.dev.config')
 
@@ -20,6 +23,21 @@ Elixir.webpack.mergeConfig(webpackDevConfig)
  | file for our application, as well as publishing vendor resources.
  |
  */
+
+gulp.task('webpack-dev-server', () => {
+    let config = Elixir.webpack.config
+    new WebpackDevServer(webpack(config),{
+        watchOptions: {
+            poll: true,
+            aggregateTimeout: 300
+        },
+        publicPath: config.output.publicPath,
+        noInfo: true,
+        stats: { colors: true }
+    }).listen(8080,"0.0.0.0",function () {
+        console.log("Bundling project...")
+    })
+})
 
 elixir(mix => {
     mix.sass('./resources/assets/admin/sass/admin.scss')
