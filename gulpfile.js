6f370b83731ed871a6f5f272aa8c7662eb62ff15
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-08-02 12:34:04 -0300

    Configuração do webpack em gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const elixir = require('laravel-elixir')
+const webpackConfig = require('./webpack.config')
+const webpackDevConfig = require('./webpack.dev.config')
 
 require('laravel-elixir-vue')
+require('laravel-elixir-webpack-official')
+
+Elixir.webpack.config.module.loaders = []
+
+Elixir.webpack.mergeConfig(webpackConfig)
+Elixir.webpack.mergeConfig(webpackDevConfig)
 
 /*
  |--------------------------------------------------------------------------
