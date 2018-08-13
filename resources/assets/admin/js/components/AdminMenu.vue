42023de9589dabb8522c1ee6b7e6be53ff127df3
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-08-13 13:58:04 -0300

    Configurando logout no menu

--- resources/assets/admin/js/components/AdminMenu.vue
+++ resources/assets/admin/js/components/AdminMenu.vue
@@ -4,21 +4,36 @@
             <a :href="item.url">{{ item.name }}</a>
         </li>
     </ul>
+    <ul id="dropdown-logout" class="dropdown-content">
+        <li>
+            <a :href="config.urlLogout" @click.prevent="goToLogout()">Sair</a>
+        </li>
+        <form id="logout-form" :action="config.urlLogout" method="POST" style="display: none">
+            <input type="hidden" name="_token" :value="config.csrfToken"/>
+        </form>
+    </ul>
     <div class="navbar-fixed">
         <nav class="teal">
-            <div class="nav-wrapper container">
-                <a href="#" class="right brand-logo">Code Contas</a>
-                <a href="#" data-activates="nav-mobile" class="button-collapse">
-                    <i class="material-icons">menu</i>
-                </a>
-                <ul class="left hide-on-med-and-down">
-                    <li v-for="o in config.menus">
-                        <a v-if="o.dropdownId" class="dropdown-button" href="!#" :data-activates="o.dropdownId">
-                            {{ o.name }} <i class="material-icons right">arrow_drop_down</i>
-                        </a>
-                        <a v-else :href="o.url">{{ o.name }}</a>
-                    </li>
-                </ul>
+            <div class="nav-wrapper">
+                <div class="col s12">
+                    <a href="#" class="left brand-logo">Financeiro Admin</a>
+                    <a href="#" data-activates="nav-mobile" class="button-collapse">
+                        <i class="material-icons">menu</i>
+                    </a>
+                    <ul class="right hide-on-med-and-down">
+                        <li v-for="o in config.menus">
+                            <a v-if="o.dropdownId" class="dropdown-button" href="!#" :data-activates="o.dropdownId">
+                                {{ o.name }} <i class="material-icons right">arrow_drop_down</i>
+                            </a>
+                            <a v-else :href="o.url">{{ o.name }}</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-button" href="!#" data-activates="dropdown-logout">
+                                {{config.name}} <i class="material-icons right">arrow_drop_down</i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
                 <ul id="nav-mobile" class="side-nav">
                     <li v-for="o in config.menus">
                         <a :href="o.url">{{ o.name }}</a>
@@ -47,6 +62,11 @@
         ready(){
             $('.button-collapse').sideNav()
             $('.dropdown-button').dropdown()
+        },
+        methods: {
+            goToLogout(){
+                $('#logout-form').submit()
+            }
         }
     }
 </script>>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -37,66 +37,13 @@
                                 ['name' => "Criar conta", 'url' => '/criar']
                             ]
                         ]
-                    ]
+                    ],
+                    'urlLogout' => env('URL_ADMIN_LOGOUT')                 ,
+                    'csrfToken' => csrf_token()
                 ];
             ?>
             <admin-menu :config="{{ json_encode($menuConfig) }}"></admin-menu>
         @endif
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#admin-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('admin.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="admin-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ url(env('URL_ADMIN_LOGIN')) }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ url(env('URL_ADMIN_LOGOUT')) }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ url(env('URL_ADMIN_LOGOUT')) }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
         @yield('content')
     </div>
