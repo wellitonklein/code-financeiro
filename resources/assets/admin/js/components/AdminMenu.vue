2ec24d1247c254f53dafb62cd7450266f1c6c012
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-08-13 14:14:33 -0300

    Definindo cor da área administrativa

--- resources/assets/admin/js/components/AdminMenu.vue
+++ resources/assets/admin/js/components/AdminMenu.vue
@@ -13,7 +13,7 @@
         </form>
     </ul>
     <div class="navbar-fixed">
-        <nav class="teal">
+        <nav>
             <div class="nav-wrapper">
                 <div class="col s12">
                     <a href="#" class="left brand-logo">Financeiro Admin</a>
