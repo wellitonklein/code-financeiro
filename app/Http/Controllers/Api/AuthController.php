a1f3fa822a0a77b86e2e6ec9c70ac6c21f817b09
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-08-16 12:11:18 -0300

    Tratamento de erros de login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use CodeFin\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Lang;
 
 class AuthController extends Controller
 {
@@ -15,18 +16,50 @@ class AuthController extends Controller
     public function accessToken(Request $request){
         $this->validateLogin($request);
 
+        if ($this->hasTooManyLoginAttempts($request)){
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
         $credentials = $this->credentials($request);
 
         if ($token = Auth::guard('api')->attempt($credentials)){
             return $this->sendLoginResponse($request,$token);
         }
+
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
     }
 
     protected function sendLoginResponse(Request $request, $token)
     {
+        $this->clearLoginAttempts($request);
+
         return response()->json([
             'token' => $token
         ]);
+    }
+
+    protected function sendLockoutResponse(Request $request)
+    {
+        $seconds = $this->limiter()->availableIn(
+            $this->throttleKey($request)
+        );
+
+        $message = Lang::get('auth.throttle',['seconds' => $seconds]);
+
+        return response()->json([
+            'message' => $message
+        ],403);
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        return response()->json([
+            'message' => Lang::get('auth.failed')
+        ],401);
     }
 
     /**
