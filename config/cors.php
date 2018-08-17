b191b0eed1a91af0a68c435a24bd03d0b62e99c5
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-08-17 12:58:46 -0300

    Integrando com laravel-cors

---
+++ config/cors.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Laravel CORS
+     |--------------------------------------------------------------------------
+     |
+     | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+     | to accept any value.
+     |
+     */
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['http://localhost:3000','http://0.0.0.0:3000'],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+    'hosts' => [],
+];
+
