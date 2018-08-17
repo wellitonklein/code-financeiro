e30efde35180f4f6aeca294eb2471687427d8a4a
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-08-17 12:26:06 -0300

    Organizando rotas

---
+++ resources/assets/spa/js/router.map.js
@@ -0,0 +1,9 @@
+import LoginComponent from './components/Login.vue'
+
+export default {
+    '/login': {
+        name: 'auth.login',
+        component: LoginComponent
+    }
+
+}
--- resources/assets/spa/js/spa.js
+++ resources/assets/spa/js/spa.js
@@ -1,25 +1,7 @@
-import LoginComponent from './components/Login.vue'
-import AppComponent from './components/App.vue'
-
 require('materialize-css')
-
 window.Vue = require('vue')
 
-let VueRouter = require('vue-router')
-const router = new VueRouter()
-
-router.map({
-    '/login': {
-        name: 'auth.login',
-        component: LoginComponent
-    }
-})
-
-router.start({
-    components: {
-        'app' : AppComponent
-    }
-},'body')
+require('./router')
 
 // import Echo from "laravel-echo"
 
