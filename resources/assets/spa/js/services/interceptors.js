873ad67033f4b0afafc10c0a192fa3831166518c
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-08-21 20:47:22 -0300

    Serviço para administração do JWT Token

--- resources/assets/spa/js/services/interceptors.js
+++ resources/assets/spa/js/services/interceptors.js
@@ -1,15 +1,16 @@
+import JwtToken from './jwt-token'
 import Auth from './auth'
 import appConfig from './appConfig'
 
 Vue.http.interceptors.push((request, next) => {
-    request.headers.set('Authorization',Auth.getAuthorizationHeader())
+    request.headers.set('Authorization',JwtToken.getAuthorizationHeader())
     next()
 })
 
 Vue.http.interceptors.push((request, next) => {
     next((response) => {
         if (response.status === 401){ //token expirado
-            return Auth.refreshToken()
+            return JwtToken.refreshToken()
                 .then(() => {
                 return Vue.http(request)
                 })
