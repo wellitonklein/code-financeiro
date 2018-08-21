0156936bc9001fc2d01963c1c47c2d69ffd7837a
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-08-21 20:14:18 -0300

    Configuração do refresh token

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -66,7 +66,7 @@ class AuthController extends Controller
     {
         return response()->json([
             'message' => Lang::get('auth.failed')
-        ],401);
+        ],400);
     }
 
     /**
--- resources/assets/spa/js/services/interceptors.js
+++ resources/assets/spa/js/services/interceptors.js
@@ -4,3 +4,13 @@ Vue.http.interceptors.push((request, next) => {
     request.headers.set('Authorization',Auth.getAuthorizationHeader())
     next()
 })
+
+Vue.http.interceptors.push((request, next) => {
+    next((response) => {
+        if (response.status === 401){ //token expirado
+            return Auth.refreshToken().then(() => {
+                return Vue.http(request)
+            })
+        }
+    })
+})
