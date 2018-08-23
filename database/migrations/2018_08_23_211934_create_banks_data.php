b04ced9abe314b9422aca979d5ce38828e5faa53
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-08-23 19:01:01 -0300

    Criando bancos padrões

---
+++ database/migrations/2018_08_23_211934_create_banks_data.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBanksData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $repository = app(\CodeFin\Repositories\BankRepository::class);
+        foreach ($this->getData() as $bankArray){
+            $repository->create($bankArray);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+
+    public function getData(){
+        return [
+            [
+                'name' => 'Bitcoin Digital',
+                'logo' => new \Illuminate\Http\UploadedFile(
+                    storage_path('app/files/banks/logos/bitcoin-digital.jpg'),
+                    'bitcoin-digital.jpg'
+                )
+            ],
+            [
+                'name' => 'Conceito',
+                'logo' => new \Illuminate\Http\UploadedFile(
+                    storage_path('app/files/banks/logos/conceito.jpg'),
+                    'conceito.jpg'
+                )
+            ],
+            [
+                'name' => 'Dinheiro Criativo',
+                'logo' => new \Illuminate\Http\UploadedFile(
+                    storage_path('app/files/banks/logos/dinheiro-criativo.jpg'),
+                    'dinheiro-criativo.jpg'
+                )
+            ],
+            [
+                'name' => 'Dinheiro Criativo B',
+                'logo' => new \Illuminate\Http\UploadedFile(
+                    storage_path('app/files/banks/logos/dinheiro-criativo2.jpg'),
+                    'dinheiro-criativo2.jpg'
+                )
+            ],
+            [
+                'name' => 'Moderno Dinheiro',
+                'logo' => new \Illuminate\Http\UploadedFile(
+                    storage_path('app/files/banks/logos/moderno-dinheiro-verde.jpg'),
+                    'moderno-dinheiro-verde.jpg'
+                )
+            ],
+        ];
+    }
+}
