001c8d8b3f488ee59f57bd96d12c9ef8c7ff4ccb
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-08-24 13:38:08 -0300

    Mostrando páginas

--- resources/assets/spa/js/components/Pagination.vue
+++ resources/assets/spa/js/components/Pagination.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>{{pages}}</div>
+    <ul class="pagination">
+        <li :class="{'disabled': currentPage == 0}">
+            <a @click.prevent="previousPage(currentPage)" href="#">
+                <i class="material-icons">chevron_left</i>
+            </a>
+        </li>
+        <li v-for="o in pages" class="waves-effect" :class="{'active': currentPage == o}">
+            <a @click.prevent="setCurrentPage(o)" href="#">{{o+1}}</a>
+        </li>
+        <li :class="{'disabled': currentPage == pages-1}">
+            <a @click.prevent="nextPage(currentPage)" href="#">
+                <i class="material-icons">chevron_right</i>
+            </a>
+        </li>
+    </ul>
 </template>
 
 <script type="text/javascript">
@@ -23,6 +37,21 @@
                 var pages = Math.ceil(this.totalRecords / this.perPage)
                 return Math.max(pages,1)
             }
+        },
+        methods: {
+            previousPage(page){
+                if (this.currentPage > 0){
+                    this.currentPage--
+                }
+            },
+            setCurrentPage(page){
+                this.currentPage = page
+            },
+            nextPage(page){
+                if (this.currentPage < this.pages -1){
+                    this.currentPage++
+                }
+            },
         }
     }
 </script>
