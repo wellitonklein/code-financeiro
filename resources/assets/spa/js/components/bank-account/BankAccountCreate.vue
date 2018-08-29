7060fc79301e7e1f772da759732b82a11d0078d8
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-08-29 14:17:06 -0300

    Testando autocomplete

--- resources/assets/spa/js/components/bank-account/BankAccountCreate.vue
+++ resources/assets/spa/js/components/bank-account/BankAccountCreate.vue
@@ -4,6 +4,7 @@
     import {BankAccount,Bank} from "../../services/resources";
     import PageTitleComponent from '../PageTitle.vue'
     import 'materialize-autocomplete'
+    import _ from 'lodash'
 
     export default {
         components: {
@@ -49,11 +50,25 @@
                         dropdown: {
                             el: '#bank-id-dropdown'
                         },
-                        getData: (value,callback) => {
-                            callback(value,self.banks)
+                        getData(value,callback) {
+                            var banks = self.filterBankByName(value)
+                            banks = banks.map((o) => {
+                                return {id: o.id, text: o.name}
+                            })
+                            callback(value,banks)
+                        },
+                        onSelect(item){
+                            self.bankAccount.bank_id = item.id
+
                         }
                     })
                 })
+            },
+            filterBankByName(name){
+                var banks = _.filter(this.banks, (o) => {
+                    return _.includes(o.name.toLowerCase(), name.toLowerCase())
+                })
+                return banks
             }
         }
     }
