b4d0c8ebb2ff0fa5f5fd5dcefb397a83d9a577f1
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-09-06 12:43:17 -0300

    Componente para adicionar e editar categorias

--- resources/assets/spa/js/components/category/CategoryList.vue
+++ resources/assets/spa/js/components/category/CategoryList.vue
@@ -7,12 +7,15 @@
             <div class="card-panel z-depth-5">
                 <category-tree :categories="categories"></category-tree>
             </div>
-            <div class="fixed-action-btn">
-                <a class="btn-floating btn-large" v-link="{name: 'bank-account.create'}">
-                    <i class="large material-icons">add</i>
-                </a>
-            </div>
-
+            <category-save :modal-options="modalOptionsSave" :category.sync="categorySave" @save-category="saveCategory">
+                <span slot="title">{{title}}</span>
+                <div slot="footer">
+                    <button type="submit" class="btn waves-effect modal-close modal-action">
+                        OK
+                    </button>
+                    <button class="btn btn-flat waves-effect waves-red modal-close modal-action">Cancelar</button>
+                </div>
+            </category-save>
         </div>
     </div>
 </template>
@@ -20,16 +23,22 @@
 <script type="text/javascript">
     import PageTitleComponent from '../PageTitle.vue'
     import CategoryTreeComponent from './CategoryTree.vue'
+    import CategorySaveComponent from './CategorySave.vue'
     import {Category} from "../../services/resources"
 
     export default {
         components: {
             'page-title': PageTitleComponent,
             'category-tree': CategoryTreeComponent,
+            'category-save': CategorySaveComponent
         },
         data(){
             return {
-                categories: []
+                categories: [],
+                title: 'Adicionar categoria',
+                modalOptionsSave: {
+                    id: 'modal-category-save'
+                }
             }
         },
         created(){
@@ -40,6 +49,9 @@
                 Category.query().then(response => {
                     this.categories = response.data.data
                 })
+            },
+            saveCategory(){
+                console.log('teeste')
             }
         }
     }
