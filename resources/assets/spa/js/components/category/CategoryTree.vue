45fd6d406802da84e78c9e22ad2fb8d0a54a06d0
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-09-06 12:24:10 -0300

    Criando ações para categorias

--- resources/assets/spa/js/components/category/CategoryTree.vue
+++ resources/assets/spa/js/components/category/CategoryTree.vue
@@ -2,12 +2,23 @@
     <ul class="category-tree">
         <li v-for="(index,o) in categories" class="category-child">
             <div class="valign-wrapper">
-                <a href="#" class="category-symbol"
-                   :class="{'color-default': o.children.data.length > 0, 'grey-text': !o.children.data.length}"
-                >
+                <a :data-activates="dropDownId(o)"
+                   href="#" class="category-symbol"
+                   :class="{'color-default': o.children.data.length > 0, 'grey-text': !o.children.data.length}">
                     <i class="material-icons">{{categoryIcon(o)}}</i>
                 </a>
-                <span class="valign">{{{ categoryText(o) }}}</span>
+                <ul :id="dropDownId(o)" class="dropdown-content">
+                    <li>
+                        <a href="#">Adicionar</a>
+                    </li>
+                    <li>
+                        <a href="#">Editar</a>
+                    </li>
+                    <li>
+                        <a href="#">Excluir</a>
+                    </li>
+                </ul>
+                <span class="valign">{{{categoryText(o)}}}</span>
             </div>
             <category-tree :categories="o.children.data"></category-tree>
         </li>
@@ -24,12 +35,28 @@
                 required: true
             }
         },
+        watch: {
+            categories:{
+                handler(categories){
+                    $('.category-child > div > a').dropdown({
+                        hover: true,
+                        inDuration: 300,
+                        outDuration: 400,
+                        belowOrigin: true
+                    })
+                },
+                deep: true
+            }
+        },
         methods:{
+            dropDownId(category){
+                return `category-tree-dropdown-${category.id}`
+            },
             categoryText(category){
                 return category.children.data.length > 0 ? `<strong>${category.name}</strong>` : category.name
             },
             categoryIcon(category){
-                return category.children.data.length > 0 ? 'folder' : 'label'
+                return category.children.data.length > 0 ? 'face' : 'child_care'
             }
         }
     }
