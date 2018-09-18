3e71fa80c161ece14758e5e33e87e6cba07d0b8d
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-09-18 12:09:46 -0300

    Introdução aos componentes de conta a pagar

---
+++ resources/assets/spa/js/components/bill/bill-pay/BillPayCreate.vue
@@ -0,0 +1,18 @@
+<template src="../_form.html"></template>
+
+<script type="text/javascript">
+    import billPayMix from '../../../mixins/bill-mixin'
+
+    export default {
+        name: "BillPayCreate",
+        mixins: [billPayMix],
+        methods:{
+            namespace(){
+                return 'billPay'
+            },
+            title(){
+                return 'Novo pagamento'
+            }
+        }
+    }
+</script>
