092ae2199408b37bf34eed2e2fd109d3ab3fc2df
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-09-19 16:07:56 -0300

    Formatando informações que serão enviadas

---
+++ resources/assets/spa/js/models/bill-pay.js
@@ -0,0 +1,38 @@
+export default class BillPay {
+    constructor(data = {}){
+        this.init()
+        Object.assign(this, data)
+    }
+
+    init(){
+        this.id = 0
+        this.date_due = ''
+        this.name = ''
+        this.value = 0
+        this.done = false
+        this.bank_account_id = ''
+        this.category_id = 0
+        this.repeat = false
+        this.repeat_type = 1
+        this.repeat_number = 0
+    }
+
+    toJSON(){
+        let o = {
+            date_due: this.date_due.toISOString().substring(0,10),
+            name: this.name,
+            value: this.value,
+            done: this.done,
+            bank_account_id: this.bank_account_id,
+            category_id: this.category_id,
+        }
+        
+        if (this.repeat){
+            o.repeat = this.repeat
+            o.repeat_type = this.repeat_type
+            o.repeat_number = this.repeat_number
+        }
+
+        return o
+    }
+}
