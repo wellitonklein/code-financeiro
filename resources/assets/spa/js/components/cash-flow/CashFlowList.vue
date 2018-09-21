c0f18ba7fab34a68c5565af800f8c008799310fa
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-09-21 15:26:33 -0300

    Download do CSV de fluxo de caixa

--- resources/assets/spa/js/components/cash-flow/CashFlowList.vue
+++ resources/assets/spa/js/components/cash-flow/CashFlowList.vue
@@ -3,6 +3,7 @@
 <script type="text/javascript">
     import PageTitleComponent from '../PageTitle.vue'
     import store from '../../store/store'
+    import Papa from 'papaparse'
 
     export default {
         name: 'CashFlowList',
@@ -54,7 +55,42 @@
             },
             categoryTotal(category, monthYear){
                 return store.getters['cashFlow/categoryTotal'](category, monthYear)
-            }
+            },
+            isCurrentMonthYear(monthYear){
+                return this.$options.filters.monthYear(new Date) === this.$options.filters.monthYear(monthYear)
+            },
+            getCsv(){
+                let csvResult = []
+                csvResult.push([])
+                $('table thead .text-csv').each((key, item) => {
+                    csvResult[0].push($(item).text().trim())
+                })
+
+                $('table tbody tr').each((key,tr) => {
+                    csvResult.push([])
+                    $(tr).find('.text-csv').each((k, element) => {
+                        csvResult[csvResult.length-1].push($(element).text().trim())
+                    })
+                })
+                return Papa.unparse(csvResult)
+            },
+            downloadCsv(){
+                let anchor = $('<a/>')
+                anchor.css('display','none')
+                anchor.attr('download','fluxo-de-caixa.csv')
+                    .attr('target','_blank')
+                    .attr('href',`data:text/csv;charset=UTF-8,${encodeURIComponent(this.getCsv())}`)
+                anchor.html('Download CSV')
+                $('body').append(anchor)
+                anchor[0].click()
+                anchor.remove()
+            },
         },
     }
 </script>
+
+<style type="text/css" scoped>
+    th{
+        border-radius: 0px;
+    }
+</style>
