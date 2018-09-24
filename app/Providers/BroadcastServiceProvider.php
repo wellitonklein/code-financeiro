55fb052f68ce07ea9a5d492d8c41fa4f8434dd23
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-09-24 12:40:38 -0300

    Terminando integração do Pusher com front-end

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -14,7 +14,7 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Broadcast::routes();
+        Broadcast::routes(['middleware' => ['web', 'cors', 'auth:api']]);
 
         /*
          * Authenticate the user's personal channel...
--- resources/assets/spa/js/echo.js
+++ resources/assets/spa/js/echo.js
@@ -1,6 +1,18 @@
 import Echo from 'laravel-echo'
+import JwtToken from './services/jwt-token'
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: 'a14389d9bab3af6e5c6d'
 })
+
+const changeJwtTokenInEcho = value => {
+    window.Echo.connector.pusher.config.auth.headers['Authorization'] =
+        JwtToken.getAuthorizationHeader()
+}
+
+JwtToken.event('updateToken', value => {
+    changeJwtTokenInEcho(value)
+})
+
+changeJwtTokenInEcho(JwtToken.token)
