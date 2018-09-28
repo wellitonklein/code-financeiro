080ad8379b93ff3f4f4d83bac6ac966eb0d76835
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-09-28 12:00:37 -0300

    Criando painel de assinatura

---
+++ resources/assets/site/js/components/subscription/SubscriptionCreate.vue
@@ -0,0 +1,13 @@
+<template src="./_template.html"></template>
+
+<script type="text/javascript">
+    export default {
+        name: 'SubscriptionCreate',
+        props: ['plan'],
+        data(){
+            return {
+                payment_type: 'credit_card'
+            }
+        }
+    }
+</script>
--- resources/views/site/subscriptions/create.blade.php
+++ resources/views/site/subscriptions/create.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.site')
 
 @section('content')
-        <div class="container">
-            <div class="row">
-                Fazer assinatura
-            </div>
+    <div class="container">
+        <div class="row">
+            <subscription-create
+                    :plan="{{json_encode($plan->toArray())}}"
+            >
+
+            </subscription-create>
         </div>
+    </div>
 @endsection
