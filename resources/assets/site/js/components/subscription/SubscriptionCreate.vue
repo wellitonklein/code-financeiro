8bb0f2202a44e72c0a842088eb828a0eeeebae9c
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-09-28 14:57:25 -0300

    Token de autorização para pagamento

--- resources/assets/site/js/components/subscription/SubscriptionCreate.vue
+++ resources/assets/site/js/components/subscription/SubscriptionCreate.vue
@@ -8,11 +8,11 @@
             return {
                 payment_type: 'credit_card',
                 credit_card: {
-                    number: '',
-                    cvv: '',
-                    expiration: '',
-                    first_name: '',
-                    last_name: ''
+                    number: '4111111111111111',
+                    cvv: '123',
+                    expiration: '12/18',
+                    first_name: 'Nome',
+                    last_name: 'Sobrenome'
                 }
             }
         },
@@ -23,7 +23,27 @@
         },
         methods: {
             submit(){
+                if (this.payment_type === 'credit_card'){
+                    let expirationArray = this.credit_card.expiration.split('/')
+                    let creditCard = Iugu.CreditCard(
+                        this.credit_card.number,
+                        expirationArray[0],
+                        expirationArray[1],
+                        this.credit_card.first_name,
+                        this.credit_card.last_name,
+                        this.credit_card.cvv,
+                    )
 
+                    Iugu.createPaymentToken(creditCard, response => {
+                        if (response.errors){
+                            Materialize.toast('Erro ao processar cartão de crédito. Tente novamente mais tarde!', 4000)
+                        }else{
+                            console.log(response.id)
+                        }
+                    })
+                }else{
+
+                }
             }
         }
     }
