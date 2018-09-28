af2d419881f260d3bfd5884b343601c849366662
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-09-28 10:20:36 -0300

    Home do site

--- resources/assets/site/js/components/SiteMenu.vue
+++ resources/assets/site/js/components/SiteMenu.vue
@@ -16,23 +16,25 @@
         <nav>
             <div class="nav-wrapper">
                 <div class="col s12">
-                    <a href="#" class="left brand-logo">Financeiro Admin</a>
-                    <a href="#" data-activates="nav-mobile" class="button-collapse">
-                        <i class="material-icons">menu</i>
-                    </a>
-                    <ul class="right hide-on-med-and-down">
-                        <li v-for="o in config.menus" :class="menuItemClass(o)">
-                            <a v-if="o.dropdownId" class="dropdown-button" href="!#" :data-activates="o.dropdownId">
-                                {{ o.name }} <i class="material-icons right">arrow_drop_down</i>
-                            </a>
-                            <a v-else :href="o.url">{{ o.name }}</a>
-                        </li>
-                        <li v-if="config.auth">
-                            <a class="dropdown-button" href="!#" data-activates="dropdown-logout">
-                                {{config.name}} <i class="material-icons right">arrow_drop_down</i>
-                            </a>
-                        </li>
-                    </ul>
+                    <div class="container">
+                        <a href="#" class="left brand-logo">Fácil Financeiro</a>
+                        <a href="#" data-activates="nav-mobile" class="button-collapse">
+                            <i class="material-icons">menu</i>
+                        </a>
+                        <ul class="right hide-on-med-and-down">
+                            <li v-for="o in config.menus" :class="menuItemClass(o)">
+                                <a v-if="o.dropdownId" class="dropdown-button" href="!#" :data-activates="o.dropdownId">
+                                    {{ o.name }} <i class="material-icons right">arrow_drop_down</i>
+                                </a>
+                                <a v-else :href="o.url">{{ o.name }}</a>
+                            </li>
+                            <li v-if="config.auth">
+                                <a class="dropdown-button" href="!#" data-activates="dropdown-logout">
+                                    {{config.name}} <i class="material-icons right">arrow_drop_down</i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <ul id="nav-mobile" class="side-nav">
                     <li v-for="o in config.menus">
---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.site')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col s12 center-align">
+            <h1>
+                <strong>Fácil Financeiro</strong>
+                <span> é uma excelente aplicação para controle financeiro</span>
+            </h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12">
+            <div class="row card z-depth-5">
+                <div class="card-content white-text center-align blue-grey">
+                    <span class="card-title">Empresarial</span>
+                </div>
+                <div class="card-content maiin-content white center-align">
+                    <p>
+                        <span>R$ 25,00/mês</span>
+                    </p>
+                    <ul class="collection" id="plan-business">
+                        <li class="collection-item">Contas a pagar</li>
+                        <li class="collection-item">Contas a receber</li>
+                        <li class="collection-item">Contas bancárias</li>
+                        <li class="collection-item">Extrato</li>
+                        <li class="collection-item">Fluxo de Caixa Anual</li>
+                        <li class="collection-item">Gráfico de Fluxo de Caixa Mensal</li>
+                        <li class="collection-item">Notificação de saldo em tempo real</li>
+                        <li class="collection-item">Filtros personalizados por período</li>
+                    </ul>
+                </div>
+                <div class="card-action white center-align">
+                    <a href="{{-- route('site.subscriptions.create') --}}" class="btn btn-large waves-effect waves-light blue-grey darken-2">
+                        Contratar
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
